95407f6f9b8e7a10b1fd95a826bc300221fee3f6
goranjovic55 <goran.jovic55@gmail.com>
2021-06-06 18:09:55 +0200

    changed pausebot mod to sell lol

--- pausebotmod.py
+++ pausebotmod.py
@@ -13,12 +13,12 @@ import os
 import time
 import threading
 
-INTERVAL = Interval.INTERVAL_5_MINUTES #Timeframe for analysis
+INTERVAL = Interval.INTERVAL_1_MINUTE #Timeframe for analysis
 
 EXCHANGE = 'BINANCE'
 SCREENER = 'CRYPTO'
 SYMBOL = parsed_config['trading_options']['EXCHANGE']
-TYPE = 'BUY'
+TYPE = 'SELL'
 THRESHOLD = 7 # 7 of 15 MA's indicating sell
 TIME_TO_WAIT = parsed_config['trading_options']['TIME_DIFFERENCE'] # Minutes to wait between analysis
 FULL_LOG = parsed_config['trading_options']['VERBOSE_MODE'] # List analysis result to console
@@ -26,21 +26,21 @@ FULL_LOG = parsed_config['trading_options']['VERBOSE_MODE'] # List analysis resu
 def analyze():
     analysis = {}
     handler = {}
-    
+
     handler = TA_Handler(
             symbol=SYMBOL,
             exchange=EXCHANGE,
             screener=SCREENER,
             interval=INTERVAL,
             timeout= 10)
- 
+
     try:
         analysis = handler.get_analysis()
     except Exception as e:
         print("pausebotmod:")
         print("Exception:")
         print(e)
-    
+
     ma_analysis = analysis.moving_averages[TYPE]
     if ma_analysis >= THRESHOLD:
         paused = True
@@ -52,7 +52,7 @@ def analyze():
     return paused
 #if __name__ == '__main__':
 def do_work():
-      
+
     while True:
         if not threading.main_thread().is_alive(): exit()
         # print(f'pausebotmod: Fetching market state')
@@ -63,6 +63,6 @@ def do_work():
         else:
             if os.path.isfile("signals/paused.exc"):
                 os.remove('signals/paused.exc')
-                        
-        # print(f'pausebotmod: Waiting {TIME_TO_WAIT} minutes for next market checkup')    
+
+        # print(f'pausebotmod: Waiting {TIME_TO_WAIT} minutes for next market checkup')
         time.sleep((TIME_TO_WAIT*60))
