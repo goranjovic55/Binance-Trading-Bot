d99d670483eab567efb0914535f1346f88318245
Row-Bear <60758353+Row-Bear@users.noreply.github.com>
2021-06-20 00:26:43 +0200

    Update Binance_Detect_Moonings.py

--- Binance_Detect_Moonings.py
+++ Binance_Detect_Moonings.py
@@ -93,7 +93,6 @@ session_struct = {
      'exchange_symbol': 'USDT',
      'price_list_counter': 0,
      'CURRENT_EXPOSURE': 0,
-     'TOTAL_GAINS': 0,
      'NEW_BALANCE': 0,
      'INVESTMENT_GAIN': 0,
      'STARTUP': True,
@@ -141,6 +140,11 @@ def decimals():
     else:
         return 8
 
+def txcolor(input):
+    if input >= 0:
+        return txcolors.SELL_PROFIT
+    else:
+        return txcolors.SELL_LOSS
 
 def get_price(add_to_historical=True):
     '''Return the current price for all coins on binance'''
@@ -650,9 +654,7 @@ def report(type, reportline):
     WON = session_struct['win_trade_count']
     LOST = session_struct['loss_trade_count']
     DECIMALS = int(decimals())
-    INVESTMENT_TOTAL = round((QUANTITY * TRADE_SLOTS), DECIMALS)
     CURRENT_EXPOSURE = round(session_struct['CURRENT_EXPOSURE'], DECIMALS)
-    session_struct['TOTAL_GAINS'] = round(session_struct['TOTAL_GAINS'], DECIMALS)
     INVESTMENT_VALUE_GAIN = round(session_struct['investment_value_gain'], 2)
 
     # testing:
@@ -660,7 +662,7 @@ def report(type, reportline):
     INVESTMENT_VALUE_TRIM =  "%g" % round(session_struct['investment_value'], 2)
     INVESTMENT_VALUE_GAIN_TRIM =  "%g" % round(session_struct['investment_value_gain'], 2)
     CURRENT_EXPOSURE_TRIM = "%g" % session_struct['CURRENT_EXPOSURE']
-    INVESTMENT_TOTAL_TRIM = "%g" % INVESTMENT_TOTAL
+    INVESTMENT_TRIM = "%g" % INVESTMENT
     CLOSED_TRADES_PERCENT_TRIM = "%g" % round(session_struct['closed_trades_percent'], 2)
     SESSION_PROFIT_TRIM = format(session_struct['session_profit'], '.8f')
     # SESSION_PROFIT_TRIM = "%g" % round(session_profit, DECIMALS)
@@ -677,11 +679,11 @@ def report(type, reportline):
         WIN_LOSS_PERCENT = 100
 
     # adding all the stats together:
-    report_string= 'Trade slots: '+str(len(coins_bought))+'/'+str(TRADE_SLOTS)+' ('+str(CURRENT_EXPOSURE_TRIM)+'/'+str(INVESTMENT_TOTAL_TRIM)+' '+PAIR_WITH+') | Session: '+str(SESSION_PROFIT_TRIM)+' '+PAIR_WITH+' ('+str(CLOSED_TRADES_PERCENT_TRIM)+'%) | Win/Loss: '+str(WON)+'/'+str(LOST)+' ('+str(WIN_LOSS_PERCENT)+'%) | Gains: '+str(round(session_struct['INVESTMENT_GAIN'], 4))+'%'+' | Balance: '+str(NEW_BALANCE_TRIM)+' | Value: '+str(INVESTMENT_VALUE_TRIM)+' USD | Value gain: '+str(INVESTMENT_VALUE_GAIN_TRIM)+' | Uptime: '+str(timedelta(seconds=(int(session_struct['session_uptime']/1000))))
+    report_string = 'Trade slots: '+str(len(coins_bought))+'/'+str(TRADE_SLOTS)+' ('+str(CURRENT_EXPOSURE_TRIM)+'/'+str(INVESTMENT_TRIM)+' '+PAIR_WITH+') | Session: '+str(SESSION_PROFIT_TRIM)+' '+PAIR_WITH+' ('+str(CLOSED_TRADES_PERCENT_TRIM)+'%) | Win/Loss: '+str(WON)+'/'+str(LOST)+' ('+str(WIN_LOSS_PERCENT)+'%) | Gains: '+str(round(session_struct['INVESTMENT_GAIN'], 4))+'%'+' | Balance: '+str(NEW_BALANCE_TRIM)+' | Value: '+str(INVESTMENT_VALUE_TRIM)+' USD | Value gain: '+str(INVESTMENT_VALUE_GAIN_TRIM)+' | Uptime: '+str(timedelta(seconds=(int(session_struct['session_uptime']/1000))))
 
     #gogo MOD todo more verbose having all the report things in it!!!!!
     if type == 'console':
-        # print(f"{txcolors.NOTICE}>> Using {len(coins_bought)}/{TRADE_SLOTS} trade slots. OT:{UNREALISED_PERCENT:.2f}%> SP:{session_profit:.2f}%> Est:{TOTAL_GAINS:.{decimals()}f} {PAIR_WITH}> W:{win_trade_count}> L:{loss_trade_count}> IT:{INVESTMENT:.{decimals()}f} {PAIR_WITH}> CE:{CURRENT_EXPOSURE:.{decimals()}f} {PAIR_WITH}> NB:{NEW_BALANCE:.{decimals()}f} {PAIR_WITH}> IV:{investment_value:.2f} {exchange_symbol}> IG:{INVESTMENT_GAIN:.2f}%> IVG:{investment_value_gain:.{decimals()}f} {exchange_symbol}> {reportline} <<{txcolors.DEFAULT}")
+        # print(f"{txcolors.NOTICE}>> Using {len(coins_bought)}/{TRADE_SLOTS} trade slots. OT:{UNREALISED_PERCENT:.2f}%> SP:{session_profit:.2f}%> W:{win_trade_count}> L:{loss_trade_count}> IT:{INVESTMENT:.{decimals()}f} {PAIR_WITH}> CE:{CURRENT_EXPOSURE:.{decimals()}f} {PAIR_WITH}> NB:{NEW_BALANCE:.{decimals()}f} {PAIR_WITH}> IV:{investment_value:.2f} {exchange_symbol}> IG:{INVESTMENT_GAIN:.2f}%> IVG:{investment_value_gain:.{decimals()}f} {exchange_symbol}> {reportline} <<{txcolors.DEFAULT}")
         print(f"{report_string}")
 
     #More detailed/verbose report style
@@ -690,12 +692,10 @@ def report(type, reportline):
         ,f"Profit on unsold coins:  {txcolors.SELL_PROFIT if UNREALISED_PERCENT >= 0 else txcolors.SELL_LOSS}{UNREALISED_PERCENT:.2f}%\n"
         ,f"Closed trades:           {txcolors.SELL_PROFIT if session_struct['closed_trades_percent'] >= 0 else txcolors.SELL_LOSS}{str(CLOSED_TRADES_PERCENT_TRIM)}%\n"
         ,f"Session profit:          {txcolors.SELL_PROFIT if session_struct['session_profit'] >= 0 else txcolors.SELL_LOSS}{str(SESSION_PROFIT_TRIM)} {PAIR_WITH}\n"
-        ,f"Est. total gains:        {txcolors.SELL_PROFIT if session_struct['TOTAL_GAINS'] >= 0 else txcolors.SELL_LOSS}{session_struct['TOTAL_GAINS']:g} {PAIR_WITH}\n"
         ,f"Trades won/lost:         {txcolors.SELL_PROFIT if session_struct['win_trade_count'] >= session_struct['loss_trade_count'] else txcolors.SELL_LOSS}{session_struct['win_trade_count']} / {txcolors.SELL_PROFIT if session_struct['win_trade_count'] >= session_struct['loss_trade_count'] else txcolors.SELL_LOSS}{session_struct['loss_trade_count']}\n"
-        ,f"Investment:              {txcolors.DEFAULT}{INVESTMENT_TOTAL:g} {PAIR_WITH}\n"
+        ,f"Investment:              {txcolors.DEFAULT}{INVESTMENT:g} {PAIR_WITH}\n"
         ,f"Current exposure:        {txcolors.DEFAULT}{session_struct['CURRENT_EXPOSURE']:g} {PAIR_WITH}\n"
-        ,f"New balance:             {txcolors.SELL_PROFIT if session_struct['NEW_BALANCE'] >= INVESTMENT_TOTAL else txcolors.SELL_LOSS}{session_struct['NEW_BALANCE']:g} {PAIR_WITH}\n"
-        ,f"Initial investment:      {txcolors.SELL_PROFIT if session_struct['investment_value'] >= INVESTMENT else txcolors.SELL_LOSS}{session_struct['investment_value']:.2f} USD\n"
+        ,f"New balance:             {txcolors.SELL_PROFIT if session_struct['NEW_BALANCE'] >= INVESTMENT else txcolors.SELL_LOSS}{session_struct['NEW_BALANCE']:g} {PAIR_WITH}\n"
         ,f"Investment gain:         {txcolors.SELL_PROFIT if session_struct['INVESTMENT_GAIN'] >= 0 else txcolors.SELL_LOSS}{session_struct['INVESTMENT_GAIN']:.2f}%\n"
         ,f"Investment value vain:   {txcolors.SELL_PROFIT if session_struct['investment_value_gain'] >= 0 else txcolors.SELL_LOSS}{str(INVESTMENT_VALUE_GAIN)} USD\n"
         ,f"{reportline} {txcolors.DEFAULT}")
@@ -791,9 +791,8 @@ def session(type):
     global session_struct
 
     if type == 'calc':
-        session_struct['TOTAL_GAINS'] = ((QUANTITY * session_struct['session_profit']) / 100)
-        session_struct['NEW_BALANCE'] = (INVESTMENT + session_struct['TOTAL_GAINS'])
-        session_struct['INVESTMENT_GAIN'] = (session_struct['TOTAL_GAINS'] / INVESTMENT) * 100
+        session_struct['NEW_BALANCE'] = (INVESTMENT + session_struct['session_profit'])
+        session_struct['INVESTMENT_GAIN'] = ((session_struct['NEW_BALANCE'] - INVESTMENT) / INVESTMENT) * 100
         session_struct['CURRENT_EXPOSURE'] = (QUANTITY * len(coins_bought))
         session_struct['unrealised_percent'] = 0
 
@@ -856,9 +855,8 @@ def session(type):
             session_struct['closed_trades_percent'] = session_info['closed_trades_percent']
             session_struct['session_uptime'] = session_info['session_uptime']
 
-        session_struct['TOTAL_GAINS'] = ((QUANTITY * session_struct['session_profit']) / 100)
-        session_struct['NEW_BALANCE'] = (INVESTMENT + session_struct['TOTAL_GAINS'])
-        session_struct['INVESTMENT_GAIN'] = (session_struct['TOTAL_GAINS'] / INVESTMENT) * 100
+        session_struct['NEW_BALANCE'] = (INVESTMENT + session_struct['session_profit'])
+        session_struct['INVESTMENT_GAIN'] = ((session_struct['NEW_BALANCE'] - INVESTMENT) / INVESTMENT) * 100
 
 
 def tickers_list(type):
