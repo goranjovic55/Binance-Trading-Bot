b73d09cdb2587507cead699e5a865990845879ce
goranjovic55 <goran.jovic55@gmail.com>
2021-07-02 17:32:42 +0200

    finally fixed stoploss calculations hopefully

--- bot/trade.py
+++ bot/trade.py
@@ -271,6 +271,7 @@ def sell_coins():
                 if coinHoldingTimeLimit < current_time: REPORT =  f"HOLDING_TIMEOUT - SELL: {coins_sold[coin]['volume']} {coin} - Bought at {buyPrice:.{decimals()}f}, sold at {lastPrice:.{decimals()}f} - Profit: {profit:.{decimals()}f} {PAIR_WITH} ({priceChange:.2f}%)"
 
                 settings_struct['STOP_LOSS'] = (settings_struct['STOP_LOSS'] + session_struct['profit_to_trade_ratio']) / 2
+                print(f"STOP_LOSS: {settings_struct['STOP_LOSS']}")
 
                 session_struct['session_profit'] = session_struct['session_profit'] + profit
                 session_struct['closed_trades_percent'] = session_struct['closed_trades_percent'] + priceChange
