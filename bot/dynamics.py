5bb39145baa9914a169023caef34fa46b2ddfd8f
goranjovic55 <goran.jovic55@gmail.com>
2021-07-03 19:34:56 +0200

    added corredct limits

--- bot/dynamics.py
+++ bot/dynamics.py
@@ -68,20 +68,6 @@ def dynamic_settings(type, TIME_DIFFERENCE, RECHECK_INTERVAL):
             session_struct['last_trade_won'] = 'none'
             print(f"{txcolors.NOTICE}>> DYNAMICS_DOWN Changing STOP_LOSS: {settings_struct['STOP_LOSS']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} - TAKE_PROFIT: {settings_struct['TAKE_PROFIT']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} - TRAILING_STOP_LOSS: {settings_struct['TRAILING_STOP_LOSS']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} CIP:{settings_struct['CHANGE_IN_PRICE_MIN']:.4f}/{settings_struct['CHANGE_IN_PRICE_MAX']:.4f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} HTL: {settings_struct['HOLDING_TIME_LIMIT']:.2f} TD: {settings_struct['TIME_DIFFERENCE']} RI: {settings_struct['RECHECK_INTERVAL']} <<{txcolors.DEFAULT}")
 
-        if type == 'reset':
-            STOP_LOSS = parsed_config['trading_options']['STOP_LOSS']
-            TAKE_PROFIT = parsed_config['trading_options']['TAKE_PROFIT']
-            TRAILING_STOP_LOSS = parsed_config['trading_options']['TRAILING_STOP_LOSS']
-            CHANGE_IN_PRICE_MAX = parsed_config['trading_options']['CHANGE_IN_PRICE_MAX']
-            CHANGE_IN_PRICE_MIN = parsed_config['trading_options']['CHANGE_IN_PRICE_MIN']
-            TIME_DIFFERENCE = parsed_config['trading_options']['TIME_DIFFERENCE']
-
-            if not TEST_MODE: HOLDING_TIME_LIMIT = (parsed_config['trading_options']['TIME_DIFFERENCE'] * 60 * 1000) * parsed_config['trading_options']['HOLDING_INTERVAL_LIMIT']
-            if TEST_MODE: HOLDING_TIME_LIMIT = (parsed_config['trading_options']['TIME_DIFFERENCE'] * 60) * parsed_config['trading_options']['HOLDING_INTERVAL_LIMIT']
-
-            print(f'{txcolors.NOTICE}>> DYNAMIC SETTINGS RESET - STOP_LOSS: {STOP_LOSS:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} - TAKE_PROFIT: {TAKE_PROFIT:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f}  - TRAILING_STOP_LOSS: {TRAILING_STOP_LOSS:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f}CIP:{CHANGE_IN_PRICE_MIN:.4f}/{CHANGE_IN_PRICE_MAX:.4f}/{DYNAMIC_WIN_LOSS_UP:.2f} HTL: {HOLDING_TIME_LIMIT:.2f}  TD: {TIME_DIFFERENCE} RI: {RECHECK_INTERVAL} <<{txcolors.DEFAULT}')
-            session_struct['dynamic'] = 'none'
-
         if type == 'mrs_settings':
            if session_struct['prices_grabbed'] == True:
               settings_struct['CHANGE_IN_PRICE_MIN'] = session_struct['market_support'] + (session_struct['market_support'] * settings_struct['DYNAMIC_CHANGE_IN_PRICE']) / 100
@@ -94,6 +80,11 @@ def dynamic_settings(type, TIME_DIFFERENCE, RECHECK_INTERVAL):
               if session_struct['win_trade_count'] > 1:
                  trading_struct['trade_resistance'] = trading_struct['sum_won_trades'] / session_struct['win_trade_count']
                  settings_struct['TRAILING_STOP_LOSS'] = trading_struct['trade_resistance']
+
+
+        STOP_LOSS = parsed_config['trading_options']['STOP_LOSS']
+        TRAILING_STOP_LOSS = parsed_config['trading_options']['TRAILING_STOP_LOSS']
+        TIME_DIFFERENCE = parsed_config['trading_options']['TIME_DIFFERENCE']
 
         #limiting STOP_LOSS TIME_DIFFERENCE and TRAILING_STOP_LOSS to dynamic min and max values
         if settings_struct['STOP_LOSS'] < STOP_LOSS / DYNAMIC_MIN_MAX:
