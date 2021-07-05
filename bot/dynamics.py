7eeee7b0794f44cdaaf803f240a1b7aacd77c87f
goranjovic55 <goran.jovic55@gmail.com>
2021-07-05 10:44:55 +0200

    changed to dynamics up and down following wins not losses

--- bot/dynamics.py
+++ bot/dynamics.py
@@ -29,16 +29,16 @@ def dynamic_settings(type, TIME_DIFFERENCE, RECHECK_INTERVAL):
 
     if DYNAMIC_SETTINGS:
 
-        if session_struct['last_trade_won'] == False and session_struct['dynamics_state'] == 'up':
+        if session_struct['last_trade_won'] == True and session_struct['dynamics_state'] == 'up':
            type = 'performance_adjust_up'
 
-        if session_struct['last_trade_won'] == False and session_struct['dynamics_state'] == 'down':
-           type = 'performance_adjust_down'
-
-        if session_struct['last_trade_won'] == True and session_struct['dynamics_state'] == 'up':
-           type = 'performance_adjust_down'
-
         if session_struct['last_trade_won'] == True and session_struct['dynamics_state'] == 'down':
+           type = 'performance_adjust_down'
+
+        if session_struct['last_trade_won'] == False and session_struct['dynamics_state'] == 'up':
+           type = 'performance_adjust_down'
+
+        if session_struct['last_trade_won'] == False and session_struct['dynamics_state'] == 'down':
            type = 'performance_adjust_up'
 
         if trading_struct['consecutive_loss'] >= 1:
