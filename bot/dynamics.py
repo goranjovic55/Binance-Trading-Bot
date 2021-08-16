f0fcd5f6d5c9d7dc50ae3d1233dba8c92540fa9d
goranjovic55 <goran.jovic55@gmail.com>
2021-08-16 20:57:48 +0200

    added comments in dynamics.py

--- bot/dynamics.py
+++ bot/dynamics.py
@@ -61,6 +61,10 @@ def dynamic_settings(type: str, TIME_DIFFERENCE: float, RECHECK_INTERVAL: float)
         if settings_struct['HOLDING_PRICE_THRESHOLD'] < HOLDING_PRICE_THRESHOLD:
            settings_struct['HOLDING_PRICE_THRESHOLD'] = HOLDING_PRICE_THRESHOLD
 
+# this part checks to see if last trade was a win if it was it checks to see what was previous dynamics state and if it was up
+# it will go up with TIMEDIFFERENCE by % percent and if it was down it will go down with it, also it will TRIGGER
+# all other settings adding % on every win
+
         if session_struct['last_trade_won'] == True:
             if session_struct['dynamics_state'] == 'up':
                settings_struct['TIME_DIFFERENCE'] = settings_struct['TIME_DIFFERENCE'] + (settings_struct['TIME_DIFFERENCE'] * DYNAMIC_WIN_LOSS_UP) /100
@@ -73,6 +77,10 @@ def dynamic_settings(type: str, TIME_DIFFERENCE: float, RECHECK_INTERVAL: float)
            session_struct['last_trade_won'] = 'none'
            type = 'performance_adjust_up'
 
+# this code will change "direction" for timedifference change aka if it was up it will go down and vice versa on next win
+# to prevent accumulating losses on same timedifference and to sync with market better, also it will subtract all other
+# dynamic settings by corresponding numberes to protect from consecutive losses
+
         if session_struct['last_trade_won'] == False:
            if session_struct['dynamics_state'] == 'up':
               session_struct['dynamics_state'] = 'down'
@@ -82,6 +90,10 @@ def dynamic_settings(type: str, TIME_DIFFERENCE: float, RECHECK_INTERVAL: float)
 
            session_struct['last_trade_won'] = 'none'
            type = 'performance_adjust_down'
+
+# this part of code jumps to different part of timedifference scale this is to protect from consecutive losses
+# and to change context so bot goes from 5 minute range to 50 minute range for example if those were corresponding
+# scale values, it jumps on 2 consecutive losses
 
         if trading_struct['consecutive_loss'] > 1:
            if settings_struct['TIME_DIFFERENCE'] > TIME_DIFFERENCE:
@@ -96,6 +108,9 @@ def dynamic_settings(type: str, TIME_DIFFERENCE: float, RECHECK_INTERVAL: float)
 
 
         #print(f'{txcolors.NOTICE}>> TRADE_WON: {session_struct['last_trade_won']} and DYNAMICS_STATE: {session_struct['dynamics_state']} <<<{txcolors.DEFAULT}')
+
+# this part of code alteres trading settings for next trade based on win/loss so if we win all our settings get more
+# if we loose they get less so we protect from consecutive losses and we are more "brave" on consecutive wins
 
         if type == 'performance_adjust_up':
             settings_struct['STOP_LOSS'] = settings_struct['STOP_LOSS'] + (settings_struct['STOP_LOSS'] * DYNAMIC_WIN_LOSS_UP) / 100
@@ -122,6 +137,8 @@ def dynamic_settings(type: str, TIME_DIFFERENCE: float, RECHECK_INTERVAL: float)
             session_struct['dynamic'] = 'none'
 
             print(f"{txcolors.NOTICE}>> DYNAMICS_DOWN Changing STOP_LOSS: {settings_struct['STOP_LOSS']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} - TAKE_PROFIT: {settings_struct['TAKE_PROFIT']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} - TRAILING_STOP_LOSS: {settings_struct['TRAILING_STOP_LOSS']:.2f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} CIP:{settings_struct['CHANGE_IN_PRICE_MIN']:.4f}/{settings_struct['CHANGE_IN_PRICE_MAX']:.4f}/{DYNAMIC_WIN_LOSS_DOWN:.2f} HTL: {settings_struct['HOLDING_TIME_LIMIT']:.2f} TD: {settings_struct['TIME_DIFFERENCE']} RI: {settings_struct['RECHECK_INTERVAL']} <<{txcolors.DEFAULT}")
+
+# this code makes our market resistance and support levels triggres for buys and also applyies our dynamics based on wins/losses
 
         if type == 'mrs_settings':
            if session_struct['prices_grabbed'] == True:
